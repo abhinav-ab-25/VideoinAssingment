ae904e55d27a2ea3f2d55fb8be189cce1e95e28f
Netprophets <netprophets@Netprophetss-MacBook-Air.local>
2024-11-13 14:42:37 +0530

    Inital Commit

---
+++ videoAssingment/videoAssingment/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  videoAssingment
+//
+//  Created by Netprophets on 13/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        
+    }
+}
+
